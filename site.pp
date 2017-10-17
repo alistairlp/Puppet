3fff353f0c9046ccf2be116b02fd0c0b8287bd56
root <root@agent4.acme.com>
2017-10-17 09:09:26 +0000

    Initial commit

---
+++ site.pp
@@ -0,0 +1,20 @@
+package { 'apache2':
+  ensure => present,
+}
+
+file { '/var/www':
+  ensure => directory,
+}
+
+file { '/var/www/html':
+  ensure => directory,
+}
+
+file { '/var/www/html/index.html':
+  ensure => file,
+  source => '/etc/puppetlabs/puppet/files/index.html',
+}
+
+service { 'apache2':
+  ensure => running,
+}
